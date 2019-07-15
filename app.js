cc0398de6b47da0d9474e77a23329a2200495b8e
Nick Foden <nickfoden@gmail.com>
2019-07-15 11:45:24 -0400

    Middleware

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const http = require("http");
 
-const routes = require("./routes");
+const express = require("express");
 
-console.log(routes.someText);
+const app = express();
 
-const server = http.createServer(routes.handler);
+app.use((req, res, next) => {
+  console.log("in middle ware");
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log("in another middle ware");
+});
+
+const server = http.createServer(app);
 
 server.listen(3000);
