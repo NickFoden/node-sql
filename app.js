d43655365b004c9d3ccbe511c1534cf0db2e38f1
Nick Foden <nickfoden@gmail.com>
2019-07-15 12:32:44 -0400

    Parsing bodies

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
-const http = require("http");
-
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("in middle ware");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    `<form action="/product" method="POST"><input type="text"' name="title"><button type="submit">Add Product</button></form>`
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("in another middle ware");
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
 });
 
-const server = http.createServer(app);
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express</h1>");
+});
 
-server.listen(3000);
+app.listen(3000);
